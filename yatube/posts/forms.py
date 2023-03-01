ab118ff623c96f56cf20f55bc96fdf2cbe55dfca
Ivan Mamichev <122726210+IvanM6767@users.noreply.github.com>
2023-03-01 21:16:47 +0300

    Update forms.py

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -6,8 +6,8 @@ from .models import Post
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
-        fields = 'text', 'group'
+        fields = ('text', 'group')
         labels = {
-            'text': ('текст'),
-            'group': ('группа'),
+            'text': 'текст',
+            'group': 'группа',
         }
